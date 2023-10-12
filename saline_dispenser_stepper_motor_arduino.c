8a204e82cb3444562203fbca3de5ca4976f0d880
Ganesh-bandi <ganeshbandi0349@gmail.com>
2023-10-12 10:01:22 +0530

    increase angle function

---
+++ saline_dispenser_stepper_motor_arduino.c
@@ -0,0 +1,61 @@
+void increase_flow()
+
+ 
+
+{
+
+ 
+
+  if(current_angle<360)
+
+ 
+
+  {
+
+ 
+
+    myStepper.setSpeed(10);
+
+ 
+
+  myStepper.step(-stepsPerRevolution);
+
+ 
+
+   current_angle=current_angle+10;
+
+ 
+
+  //delay(1000);
+
+ 
+
+  }
+
+ 
+
+  else
+
+ 
+
+  {
+
+ 
+
+      Serial.print("min value");
+
+ 
+
+      Serial.println(current_angle);
+
+ 
+
+  }
+
+ 
+
+   
+
+ 
+
+}
