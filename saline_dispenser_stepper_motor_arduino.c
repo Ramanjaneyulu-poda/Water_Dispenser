c8c38cec71b8cacc377480eca75eb01a3674dfcb
Ganesh-bandi <118507786+Ganesh-bandi@users.noreply.github.com>
2023-10-12 10:29:50 +0530

    Update saline_dispenser_stepper_motor_arduino.c

--- saline_dispenser_stepper_motor_arduino.c
+++ saline_dispenser_stepper_motor_arduino.c
@@ -1,61 +1,15 @@
 void increase_flow()
-
- 
-
 {
-
- 
-
-  if(current_angle<360)
-
- 
-
-  {
-
- 
-
-    myStepper.setSpeed(10);
-
- 
-
-  myStepper.step(-stepsPerRevolution);
-
- 
-
-   current_angle=current_angle+10;
-
- 
-
-  //delay(1000);
-
- 
-
+ if(current_angle<360)
+ {
+ myStepper.setSpeed(10);
+myStepper.step(-stepsPerRevolution);
+current_angle=current_angle+10;
+//delay(1000);
+ }
+else
+ {
+    Serial.print("min value");
+   Serial.println(current_angle);
   }
-
- 
-
-  else
-
- 
-
-  {
-
- 
-
-      Serial.print("min value");
-
- 
-
-      Serial.println(current_angle);
-
- 
-
-  }
-
- 
-
-   
-
- 
-
 }
